df537d38b490131cf71ae9d98aa8586b04a2d1d2
Cyrille Nofficial <cynoffic@cyrilix.fr>
2018-02-26 21:03:41 +0100

    [refactor] Reformat code and add logger

--- donkeycar/management/base.py
+++ donkeycar/management/base.py
@@ -1,17 +1,19 @@
-
-import sys
-import os
-import socket
-import shutil
 import argparse
+import logging
+import os
+import shutil
+import socket
+import sys
 
 import donkeycar as dk
 from donkeycar.parts.datastore import Tub
 from .tub import TubManager
 
+logger = logging.getLogger(__name__)
 
 PACKAGE_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 TEMPLATES_PATH = os.path.join(PACKAGE_PATH, 'templates')
+
 
 def make_dir(path):
     real_path = os.path.expanduser(path)
@@ -22,122 +24,119 @@ def make_dir(path):
 
 
 def load_config(config_path):
-
-    '''
+    """
     load a config from the given path
-    '''
+    """
     conf = os.path.expanduser(config_path)
 
     if not os.path.exists(conf):
-        print("No config file at location: %s. Add --config to specify\
-                location or run from dir containing config.py." % conf)
+        logger.error("No config file at location: %s. Add --config to specify\
+                location or run from dir containing config.py.", conf)
         return None
 
     try:
         cfg = dk.load_config(conf)
     except:
-        print("Exception while loading config from", conf)
+        logger.error("Exception while loading config from %s", conf)
         return None
 
     return cfg
 
 
-class BaseCommand():
+class BaseCommand:
     pass
 
 
 class CreateCar(BaseCommand):
-    
-    def parse_args(self, args):
+
+    @staticmethod
+    def parse_args(args):
         parser = argparse.ArgumentParser(prog='createcar', usage='%(prog)s [options]')
         parser.add_argument('--path', default=None, help='path where to create car folder')
         parser.add_argument('--template', default=None, help='name of car template to use')
         parser.add_argument('--overwrite', action='store_true', help='should replace existing files')
-        
+
         parsed_args = parser.parse_args(args)
         return parsed_args
-        
+
     def run(self, args):
         args = self.parse_args(args)
         self.create_car(path=args.path, template=args.template, overwrite=args.overwrite)
-    
-    def create_car(self, path, template='donkey2', overwrite=False):
+
+    @staticmethod
+    def create_car(path, template='donkey2', overwrite=False):
         """
-        This script sets up the folder struction for donkey to work. 
+        This script sets up the folder struction for donkey to work.
         It must run without donkey installed so that people installing with
         docker can build the folder structure for docker to mount to.
         """
 
-        #these are neeeded incase None is passed as path
+        # these are neeeded incase None is passed as path
         path = path or '~/d2'
         template = template or 'donkey2'
 
-
-        print("Creating car folder: {}".format(path))
+        logger.info("Creating car folder: {}".format(path))
         path = make_dir(path)
-        
-        print("Creating data & model folders.")
+
+        logger.info("Creating data & model folders.")
         folders = ['models', 'data', 'logs']
-        folder_paths = [os.path.join(path, f) for f in folders]   
+        folder_paths = [os.path.join(path, f) for f in folders]
         for fp in folder_paths:
             make_dir(fp)
-            
-        #add car application and config files if they don't exist
-        app_template_path = os.path.join(TEMPLATES_PATH, template+'.py')
+
+        # add car application and config files if they don't exist
+        app_template_path = os.path.join(TEMPLATES_PATH, template + '.py')
         config_template_path = os.path.join(TEMPLATES_PATH, 'config_defaults.py')
         car_app_path = os.path.join(path, 'manage.py')
         car_config_path = os.path.join(path, 'config.py')
-        
-        if os.path.exists(car_app_path) and not overwrite:
-            print('Car app already exists. Delete it and rerun createcar to replace.')
-        else:
-            print("Copying car application template: {}".format(template))
-            shutil.copyfile(app_template_path, car_app_path)
-            
-        if os.path.exists(car_config_path) and not overwrite:
-            print('Car config already exists. Delete it and rerun createcar to replace.')
-        else:
-            print("Copying car config defaults. Adjust these before starting your car.")
-            shutil.copyfile(config_template_path, car_config_path)
- 
-        print("Donkey setup complete.")
 
+        if os.path.exists(car_app_path) and not overwrite:
+            logger.warning('Car app already exists. Delete it and rerun createcar to replace.')
+        else:
+            logger.info("Copying car application template: %s", template)
+            shutil.copyfile(app_template_path, car_app_path)
+
+        if os.path.exists(car_config_path) and not overwrite:
+            logger.warning('Car config already exists. Delete it and rerun createcar to replace.')
+        else:
+            logger.info("Copying car config defaults. Adjust these before starting your car.")
+            shutil.copyfile(config_template_path, car_config_path)
+
+        logger.info("Donkey setup complete.")
 
 
 class UploadData(BaseCommand):
-    
-    def parse_args(self, args):
+
+    @staticmethod
+    def parse_args(args):
         parser = argparse.ArgumentParser(prog='uploaddata', usage='%(prog)s [options]')
         parser.add_argument('--url', help='path where to create car folder')
         parser.add_argument('--template', help='name of car template to use')
-        
+
         parsed_args = parser.parse_args(args)
         return parsed_args
 
 
-
 class FindCar(BaseCommand):
     def parse_args(self, args):
-        pass        
+        pass
 
-        
     def run(self, args):
         print('Looking up your computer IP address...')
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8",80))
-        ip = s.getsockname()[0] 
-        print('Your IP address: %s ' %s.getsockname()[0])
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+        print('Your IP address: %s ' % s.getsockname()[0])
         s.close()
-        
+
         print("Finding your car's IP address...")
         cmd = "sudo nmap -sP " + ip + "/24 | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}'"
-        print("Your car's ip address is:" )
+        print("Your car's ip address is:")
         os.system(cmd)
-        
-        
-        
-class CalibrateCar(BaseCommand):    
-    
+
+
+class CalibrateCar(BaseCommand):
+
     def parse_args(self, args):
         parser = argparse.ArgumentParser(prog='calibrate', usage='%(prog)s [options]')
         parser.add_argument('--channel', help='The channel youd like to calibrate [0-15]')
@@ -146,23 +145,25 @@ class CalibrateCar(BaseCommand):
 
     def run(self, args):
         from donkeycar.parts.actuator import PCA9685
-    
+
         args = self.parse_args(args)
         channel = int(args.channel)
         c = PCA9685(channel)
-        
+
         for i in range(10):
             pmw = int(input('Enter a PWM setting to test(0-1500)'))
             c.run(pmw)
 
 
-class MakeMovie(BaseCommand):    
-    
+class MakeMovie(BaseCommand):
+
     def parse_args(self, args):
         parser = argparse.ArgumentParser(prog='makemovie')
         parser.add_argument('--tub', help='The tub to make movie from')
-        parser.add_argument('--out', default='tub_movie.mp4', help='The movie filename to create. default: tub_movie.mp4')
-        parser.add_argument('--config', default='./config.py', help='location of config file to use. default: ./config.py')
+        parser.add_argument('--out', default='tub_movie.mp4',
+                            help='The movie filename to create. default: tub_movie.mp4')
+        parser.add_argument('--config', default='./config.py',
+                            help='location of config file to use. default: ./config.py')
         parsed_args = parser.parse_args(args)
         return parsed_args, parser
 
@@ -173,12 +174,11 @@ class MakeMovie(BaseCommand):
         '''
         import moviepy.editor as mpy
 
-
         args, parser = self.parse_args(args)
 
         if args.tub is None:
             parser.print_help()
-            return            
+            return
 
         conf = os.path.expanduser(args.config)
 
@@ -198,8 +198,8 @@ class MakeMovie(BaseCommand):
         self.iRec = 0
 
         print('making movie', args.out, 'from', self.num_rec, 'images')
-        clip = mpy.VideoClip(self.make_frame, duration=(self.num_rec//cfg.DRIVE_LOOP_HZ) - 1)
-        clip.write_videofile(args.out,fps=cfg.DRIVE_LOOP_HZ)
+        clip = mpy.VideoClip(self.make_frame, duration=(self.num_rec // cfg.DRIVE_LOOP_HZ) - 1)
+        clip.write_videofile(args.out, fps=cfg.DRIVE_LOOP_HZ)
 
         print('done')
 
@@ -211,28 +211,26 @@ class MakeMovie(BaseCommand):
         a frame counter. This assumes sequential access.
         '''
         self.iRec = self.iRec + 1
-        
+
         if self.iRec >= self.num_rec - 1:
             return None
 
         rec = self.tub.get_record(self.iRec)
         image = rec['cam/image_array']
-        
-        return image # returns a 8-bit RGB array
 
-
-
+        return image  # returns a 8-bit RGB array
 
 
 class Sim(BaseCommand):
     '''
-    Start a websocket SocketIO server to talk to a donkey simulator    
+    Start a websocket SocketIO server to talk to a donkey simulator
     '''
-    
+
     def parse_args(self, args):
         parser = argparse.ArgumentParser(prog='sim')
         parser.add_argument('--model', help='the model to use for predictions')
-        parser.add_argument('--config', default='./config.py', help='location of config file to use. default: ./config.py')
+        parser.add_argument('--config', default='./config.py',
+                            help='location of config file to use. default: ./config.py')
         parser.add_argument('--type', default='categorical', help='model type to use when loading. categorical|linear')
         parser.add_argument('--top_speed', default='3', help='what is top speed to drive')
         parsed_args = parser.parse_args(args)
@@ -253,7 +251,7 @@ class Sim(BaseCommand):
         if cfg is None:
             return
 
-        #TODO: this logic should be in a pilot or modle handler part.
+        # TODO: this logic should be in a pilot or modle handler part.
         if args.type == "categorical":
             kl = KerasCategorical()
         elif args.type == "linear":
@@ -262,21 +260,21 @@ class Sim(BaseCommand):
             print("didn't recognice type:", args.type)
             return
 
-        #can provide an optional image filter part
+        # can provide an optional image filter part
         img_stack = None
 
-        #load keras model
-        kl.load(args.model)  
+        # load keras model
+        kl.load(args.model)
 
-        #start socket server framework
+        # start socket server framework
         sio = socketio.Server()
 
         top_speed = float(args.top_speed)
 
-        #start sim server handler
+        # start sim server handler
         ss = SteeringServer(sio, kpart=kl, top_speed=top_speed, image_part=img_stack)
-                
-        #register events and pass to server handlers
+
+        # register events and pass to server handlers
 
         @sio.on('telemetry')
         def telemetry(sid, data):
@@ -287,7 +285,6 @@ class Sim(BaseCommand):
             ss.connect(sid, environ)
 
         ss.go(('0.0.0.0', 9090))
-
 
 
 class TubCheck(BaseCommand):
@@ -348,8 +345,6 @@ class ShowPredictionPlots(BaseCommand):
         Plot model predictions for angle and throttle against data from tubs.
 
         '''
-        import matplotlib.pyplot as plt
-        import pandas as pd
         from donkeycar.parts.datastore import TubGroup
         from donkeycar.parts.keras import KerasCategorical
 
@@ -359,7 +354,7 @@ class ShowPredictionPlots(BaseCommand):
         model = KerasCategorical()
         model.load(model_path)
 
-        gen = tg.get_batch_gen(batch_size=len(tg.df),shuffle=False)
+        gen = tg.get_batch_gen(batch_size=len(tg.df), shuffle=False)
         arr = next(gen)
 
         """
@@ -401,25 +396,26 @@ class ShowPredictionPlots(BaseCommand):
         plt.show()
         """
 
+
 def execute_from_command_line():
     """
     This is the fuction linked to the "donkey" terminal command.
     """
     commands = {
-            'createcar': CreateCar,
-            'findcar': FindCar,
-            'calibrate': CalibrateCar,
-            'tubclean': TubManager,
-            'tubhist': ShowHistogram,
-            'tubplot': ShowPredictionPlots,
-            'tubcheck': TubCheck,
-            'makemovie': MakeMovie,
-            'sim': Sim,
-                }
-    
+        'createcar': CreateCar,
+        'findcar': FindCar,
+        'calibrate': CalibrateCar,
+        'tubclean': TubManager,
+        'tubhist': ShowHistogram,
+        'tubplot': ShowPredictionPlots,
+        'tubcheck': TubCheck,
+        'makemovie': MakeMovie,
+        'sim': Sim,
+    }
+
     args = sys.argv[:]
     command_text = args[1]
-    
+
     if command_text in commands.keys():
         command = commands[command_text]
         c = command()
@@ -427,6 +423,3 @@ def execute_from_command_line():
     else:
         dk.utils.eprint('Usage: The availible commands are:')
         dk.utils.eprint(list(commands.keys()))
-        
-    
-    
