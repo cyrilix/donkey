a63169e352851d8180c7a6248578c690fcf0618e
Cyrille Nofficial <cynoffic@cyrilix.fr>
2018-03-15 19:47:06 +0100

    [diabolo] Camera rotation

--- donkeycar/templates/diabolo.py
+++ donkeycar/templates/diabolo.py
@@ -27,7 +27,7 @@ class Diabolo(BaseVehicle):
         self.add(GpioMotor(), inputs=['throttle', 'angle'])
 
     def _configure_camera(self, cfg):
-        cam = PiCamera(resolution=cfg.CAMERA_RESOLUTION)
+        cam = PiCamera(resolution=cfg.CAMERA_RESOLUTION, rotation=180)
         self.add(cam, outputs=['cam/image_array'], threaded=True)
 
 
