ad7b0e6642c05a1796eaa378909eadf3c0836060
Cyrille Nofficial <cynoffic@cyrilix.fr>
2018-04-01 19:27:28 +0200

    [LocalWebController] Implement missing method

--- donkeycar/parts/web_controller/web.py
+++ donkeycar/parts/web_controller/web.py
@@ -141,6 +141,9 @@ class LocalWebController(tornado.web.Application):
         self.img_arr = img_arr
         return self.angle, self.throttle, self.mode, self.recording
 
+    def shutdown(self):
+        pass
+
 
 class DriveAPI(tornado.web.RequestHandler):
 
