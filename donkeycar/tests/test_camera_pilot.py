a04c9de870b48c5c055850ce3df81730d21e9912
Cyrille Nofficial <cynoffic@cyrilix.fr>
2018-04-02 12:06:57 +0200

    [Shock detection] Add unit tests

--- donkeycar/tests/test_camera_pilot.py
+++ donkeycar/tests/test_camera_pilot.py
@@ -28,27 +28,39 @@ def _load_img_gray(img):
 class TestDrive:
 
     def test_blank(self, pilot):
-        assert pilot.run([]) == (0.0, 0.1)
-        assert pilot.run(None) == (0.0, 0.1)
+        assert pilot.run([], False) == (0.0, 0.1)
+        assert pilot.run(None, False) == (0.0, 0.1)
 
     def test_on_error(self, pilot):
         def throw_error(centroids):
             raise ValueError()
 
         pilot._angle_estimator.__dict__['estimate'] = throw_error
-        assert pilot.run([(10, 20)]) == (0.0, 0.0)
+        assert pilot.run([(10, 20)], False) == (0.0, 0.0)
 
     def test_straight_line(self, pilot):
-        angle, throttle = pilot.run([(75, 110), (80, 85)])
+        angle, throttle = pilot.run([(75, 110), (80, 85)], False)
 
         assert throttle == 0.1
         assert 0.1 >= angle
 
     def test_turn_right(self, pilot):
-        angle, throttle = pilot.run([(150, 150), (150, 140)])
+        angle, throttle = pilot.run([(150, 150), (150, 140)], False)
         assert throttle == 0.1
         assert 0.9 >= angle >= 0.2
 
+    def test_shock(self, pilot):
+        angle, throttle = pilot.run([(150, 150), (150, 140)], False)
+        assert throttle == 0.1
+        assert 0.9 >= angle >= 0.2
+
+        angle, throttle = pilot.run([(150, 150), (150, 140)], True)
+        assert throttle == 0.0
+        assert 0.9 >= angle >= 0.2
+
+        angle, throttle = pilot.run([(150, 150), (150, 140)], False)
+        assert throttle == 0.0
+        assert 0.9 >= angle >= 0.2
 
 @pytest.fixture()
 def angle_processor():
@@ -93,8 +105,14 @@ class TestAngleEstimatorMiddleLine:
 class TestThrottleControllerFixedSpeed:
 
     def test_run(self):
-        assert ThrottleControllerFixedSpeed(throttle_value=0.1).run([]) == 0.1
-        assert ThrottleControllerFixedSpeed(throttle_value=0.8).run([]) == 0.8
+        assert ThrottleControllerFixedSpeed(throttle_value=0.1).run([], False) == 0.1
+        assert ThrottleControllerFixedSpeed(throttle_value=0.8).run([], False) == 0.8
+
+    def test_throttle_with_shock(self):
+        controller_fixed_speed = ThrottleControllerFixedSpeed(throttle_value=0.1)
+        assert controller_fixed_speed.run([], False) == 0.1
+        assert controller_fixed_speed.run([], True) == 0.0
+        assert controller_fixed_speed.run([], False) == 0.0
 
 
 @pytest.fixture
@@ -142,13 +160,17 @@ def throttle_controller_angle():
 class TestThrottleControllerSteeringBased:
 
     def test_throttle_with_min_angle(self, throttle_controller_angle):
-        assert throttle_controller_angle.run(0.0) == 0.8
+        assert throttle_controller_angle.run(0.0, False) == 0.8
 
     def test_throttle_with_max_angle(self, throttle_controller_angle):
-        assert throttle_controller_angle.run(1.0) == 0.4
+        assert throttle_controller_angle.run(1.0, False) == 0.4
 
     def test_throttle_with_intermediate_angle(self, throttle_controller_angle):
-        assert throttle_controller_angle.run(0.5) == 0.6
-        assert throttle_controller_angle.run(0.8) == 0.72
-        assert throttle_controller_angle.run(0.2) == 0.48
+        assert throttle_controller_angle.run(0.5, False) == 0.6
+        assert throttle_controller_angle.run(0.8, False) == 0.72
+        assert throttle_controller_angle.run(0.2, False) == 0.48
 
+    def test_throttle_with_shock(self, throttle_controller_angle):
+        assert throttle_controller_angle.run(1.0, False) > 0.1
+        assert throttle_controller_angle.run(1.0, True) == 0.0
+        assert throttle_controller_angle.run(1.0, False) == 0.0
