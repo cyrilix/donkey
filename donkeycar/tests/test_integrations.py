532ca6858ba88698c79c866331bc3df103a7b275
Cyrille Nofficial <cynoffic@cyrilix.fr>
2018-04-29 17:18:23 +0200

    [test] Add integration tests

---
+++ donkeycar/tests/test_integrations.py
@@ -0,0 +1,36 @@
+from typing import List, Dict
+
+import pytest
+from pytest_docker_compose import NetworkInfo
+
+import donkeycar as dk
+from donkeycar.parts.camera import ImageListCamera
+from donkeycar.templates.fousduvolant_base import BaseVehicle
+from donkeycar.tests.conftest import wait_port_open
+
+
+class MalabilleCar(BaseVehicle):
+    def _configure_car_hardware(self, cfg):
+        pass
+
+    def _configure_camera(self, cfg):
+        camera = ImageListCamera(path_mask='test_parts/*.jpg')
+        self.add(camera, outputs=['cam/image_array'], threaded=True)
+
+    def _configure_arduino(self, cfg):
+        pass
+
+
+@pytest.fixture(name='car')
+def fixture_car(docker_network_info: Dict[str, List[NetworkInfo]]):
+    mqtt_service = docker_network_info["donkeycar_mqtt_1"][0]
+    host = 'localhost'
+    port = 1883
+    wait_port_open(host=host, port=port)
+
+    cfg = dk.load_config(config_path='donkeycar/templates/config_defaults.py')
+    return MalabilleCar(cfg=cfg)
+
+
+def test_run(car: MalabilleCar):
+    car.start(rate_hz=20, max_loop_count=10)
